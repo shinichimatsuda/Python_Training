c11926971db89683c7e8b10037193a9882a7607d
SMatsuda <matsushin51@gmail.com>
2018-05-14 06:08:04 +0900

    Create DCamp_Computing percentiles.py

---
+++ DCamp_Computing percentiles.py
@@ -0,0 +1,9 @@
+# Specify array of percentiles: percentiles
+import numpy as np
+percentiles = np.array([2.5, 25, 50, 75, 97.5])
+
+# Compute percentiles: ptiles_vers
+ptiles_vers = np.percentile(versicolor_petal_length, percentiles)
+
+# Print the result
+print(ptiles_vers)
