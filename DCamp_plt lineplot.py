94d8057cb872f6d6682541003781aa08e22ae707
SMatsuda <matsushin51@gmail.com>
2018-05-14 12:57:12 +0900

    Update and rename plt lineplot.py to DCamp_plt lineplot.py

--- DCamp_plt lineplot.py
+++ DCamp_plt lineplot.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 
 # Print the last item of gdp_cap and life_exp
 print(gdp_cap[-1], life_exp[-1])
-print(life_exp[-1])
+print(life_exp[-1])   ### For an automatic answer checking purpose, this line was inserted.
 
 # Make a line plot, gdp_cap on the x-axis, life_exp on the y-axis
 _ = plt.plot(gdp_cap, life_exp)
