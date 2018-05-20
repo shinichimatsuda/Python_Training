65975f8e74009333e4bf19766b4dd72a67fd2f56
SMatsuda <matsushin51@gmail.com>
2018-05-20 16:58:16 +0900

    Create DCamp_ECDF simulation.py

---
+++ DCamp_ECDF simulation.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Compute ECDF: x, y
+x, y = ecdf(n_defaults)
+
+# Plot the ECDF with labeled axes
+_ = plt.plot(x, y, marker='.', linestyle='none')
+_ = plt.xlabel('number of defaults out of 100')
+_ = plt.ylabel('ECDF')
+
+# Show the plot
+plt.show()
+
+# Compute the number of 100-loan simulations with 10 or more defaults: n_lose_money
+n_lose_money = np.sum(n_defaults >= 10)
+
+# Compute and print probability of losing money
+print('Probability of losing money =', n_lose_money / len(n_defaults))
