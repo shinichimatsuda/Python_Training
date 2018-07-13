9c5d8f420e095f6fc54ed6a793c55678b8246e47
SMatsuda <matsushin51@gmail.com>
2018-07-13 23:52:13 +0900

    Create DCamp_Cleaning visualizing histogram.py

---
+++ DCamp_Cleaning visualizing histogram.py
@@ -0,0 +1,10 @@
+# Import matplotlib.pyplot
+import matplotlib.pyplot as plt
+
+print(df['Existing Zoning Sqft'].describe())
+
+# Plot the histogram
+df['Existing Zoning Sqft'].plot(kind='hist', rot=70, logx=True, logy=True)
+
+# Display the histogram
+plt.show()
