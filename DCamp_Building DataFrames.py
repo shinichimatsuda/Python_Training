c2feb8ab04b1c2b2da51a03125dd84be3b8ba27e
SMatsuda <matsushin51@gmail.com>
2018-07-16 09:23:54 +0900

    Create DCamp_Building DataFrames.py

---
+++ DCamp_Building DataFrames.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+# Make a string with the value 'PA': state
+state = 'PA'
+
+# Construct a dictionary: data
+data = {'state':state, 'city':cities}
+
+# Construct a DataFrame from dictionary data: df
+df = pd.DataFrame(data)
+
+# Print the DataFrame
+print(df)
