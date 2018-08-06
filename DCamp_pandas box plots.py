2996455c187b4b503a1e4ac642bb41959d34d9fb
SMatsuda <matsushin51@gmail.com>
2018-08-07 06:23:24 +0900

    Create DCamp_pandas box plots.py

---
+++ DCamp_pandas box plots.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Make a list of the column names to be plotted: cols
+cols = ['weight','mpg']
+
+# Generate the box plots
+df[cols].plot(kind = 'box',subplots = True)
+
+# Display the plot
+plt.show()
