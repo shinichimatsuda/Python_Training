f1c2e6ddbd1c2b94cf6d3674db9563a279515964
SMatsuda <matsushin51@gmail.com>
2018-08-09 21:41:18 +0900

    Create DCamp_Standard deviation .std().py

---
+++ DCamp_Standard deviation .std().py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+# Print the mean of the January and March data
+print(january.mean(), march.mean())
+
+# Print the standard deviation of the January and March data
+print(january.std(), march.std())
