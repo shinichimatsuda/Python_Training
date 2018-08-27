396e9b80fd2272e6b4f00cb785077e5731cca2f6
SMatsuda <matsushin51@gmail.com>
2018-08-27 16:44:24 +0900

    Create DCamp_pandas plotting date ranges.py

---
+++ DCamp_pandas plotting date ranges.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Plot the summer data
+df.Temperature['2010-Jun':'2010-Aug'].plot()
+plt.show()
+plt.clf()
+
+# Plot the one week data
+df.Temperature['2010-06-10':'2010-06-17'].plot()
+plt.show()
+plt.clf()
