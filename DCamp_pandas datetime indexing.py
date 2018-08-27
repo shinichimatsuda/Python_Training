e51b742282d625d656cc6641c85b18bd84bdbacc
SMatsuda <matsushin51@gmail.com>
2018-08-27 14:33:16 +0900

    Create DCamp_pandas datetime indexing.py

---
+++ DCamp_pandas datetime indexing.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Plot the raw data before setting the datetime index
+df.plot()
+plt.show()
+
+# Convert the 'Date' column into a collection of datetime objects: df.Date
+df.Date = pd.to_datetime(df['Date'])
+
+# Set the index to be the converted 'Date' column
+df.set_index('Date', inplace = True)
+
+# Re-plot the DataFrame to see that the axis is now datetime aware!
+df.plot()
+plt.show()
