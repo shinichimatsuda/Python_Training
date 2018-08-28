9ea93250b456f3c237b476e63e327db93fff03eb
SMatsuda <matsushin51@gmail.com>
2018-08-29 06:09:52 +0900

    Create DCamp_Reshape data using melt.py

---
+++ DCamp_Reshape data using melt.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+# Print the head of airquality
+print(airquality.head())
+
+# Melt airquality: airquality_melt
+airquality_melt = pd.melt(frame = df, id_vars = ['Month', 'Day'])
+
+# Print the head of airquality_melt
+print(airquality_melt.head())
