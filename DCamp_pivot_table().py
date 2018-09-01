e5665664898adbbbcc8b4c7e12cdaaa3d51828af
SMatsuda <matsushin51@gmail.com>
2018-09-01 09:56:07 +0900

    Create DCamp_pivot_table().py

---
+++ DCamp_pivot_table().py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+# Print the head of airquality_melt
+print(airquality_melt.head())
+
+# Pivot airquality_melt: airquality_pivot
+airquality_pivot = airquality_melt.pivot_table(index = ['Month', 'Day'], columns = 'measurement', values = 'reading')
+
+# Print the head of airquality_pivot
+print(airquality_pivot.head())
