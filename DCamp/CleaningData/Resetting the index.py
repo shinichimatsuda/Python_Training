c134138163eedd6b002ae72d52a6433d18ef6111
SMatsuda <matsushin51@gmail.com>
2018-09-03 00:58:50 +0900

    Create Resetting the index.py

---
+++ DCamp/CleaningData/Resetting the index.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+# Print the index of airquality_pivot
+print(airquality_pivot.index)
+
+# Reset the index of airquality_pivot: airquality_pivot_reset
+airquality_pivot_reset = airquality_pivot.reset_index()
+
+# Print the new index of airquality_pivot_reset
+print(airquality_pivot_reset.index)
+
+# Print the head of airquality_pivot_reset
+print(airquality_pivot_reset.head())
