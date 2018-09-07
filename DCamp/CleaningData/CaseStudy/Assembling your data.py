ab700b428c46cf851ed38e16f25cebecf4ffe937
SMatsuda <matsushin51@gmail.com>
2018-09-08 06:38:04 +0900

    Create Assembling your data.py

---
+++ DCamp/CleaningData/CaseStudy/Assembling your data.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+# Concatenate the DataFrames row-wise
+gapminder = pd.concat([g1800s, g1900s, g2000s])
+
+# Print the shape of gapminder
+print(gapminder.shape)
+
+# Print the head of gapminder
+print(gapminder.head())
